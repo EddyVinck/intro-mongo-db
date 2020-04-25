8cee33684c3d52f554d752a69134c97d311019fe
EddyVinck <eddyvinck95@gmail.com>
2020-04-25 15:28:53 +0200

    associations exercise

--- exercises/queries/queries.js
+++ exercises/queries/queries.js
@@ -1,28 +1,41 @@
-const Post = require('./post')
+const Post = require("./post");
 
 const postByTitle = (title) => {
+  return Post.findOne({title}).exec();
+};
 
-}
-
-const postsForAuthor = (authorId) => {
-
-}
+const postsForAuthor = async (authorId) => {
+  return Post.find({author: authorId});
+};
 
 const fullPostById = (id) => {
-  
-}
+  return Post.findById(id).populate("author").exec();
+};
 
 const allPostsSlim = (fieldsToSelect) => {
-  
-}
+  return Post.find({}).select(fieldsToSelect).exec();
+};
 
 const postByContentLength = (maxContentLength, minContentLength) => {
-  
-}
+  return Post.find({
+    contentLength: {
+      $gt: minContentLength, // inclusive would be $gte
+      $lt: maxContentLength, // or $lte for "less than or equal to".
+    },
+  }).exec();
+};
 
 const addSimilarPosts = (postId, similarPosts) => {
-  
-}
+  return Post.findByIdAndUpdate(
+    postId,
+    {
+      // add a push command on similarPosts
+      // push each item in similarPosts
+      $push: {similarPosts: {$each: similarPosts}},
+    },
+    {new: true}
+  ).exec();
+};
 
 module.exports = {
   postByTitle,
@@ -30,5 +43,5 @@ module.exports = {
   fullPostById,
   allPostsSlim,
   postByContentLength,
-  addSimilarPosts
-}
+  addSimilarPosts,
+};
