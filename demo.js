e41ea0846f13df46a693fa4033e918bc12f1722b
EddyVinck <eddyvinck95@gmail.com>
2020-04-25 14:28:19 +0200

    more examples for queries

--- demo.js
+++ demo.js
@@ -37,6 +37,10 @@ const student = new mongoose.Schema(
 
 const school = new mongoose.Schema({
   name: String,
+  openSince: Number,
+  students: Number,
+  isGreat: Boolean,
+  staff: [{type: String}],
 });
 
 // Mongo will pluralize this: 'student' -> 'students' in the database
@@ -45,17 +49,50 @@ const School = mongoose.model("school", school);
 
 connect()
   .then(async (_conn) => {
-    const newSchool = await School.create({name: "cool school"});
-    const newStudent = await Student.create({
-      firstName: "Bobbie",
-      school: newSchool._id,
-    });
+    // const newSchool = await School.create({name: "cool school"});
+    // const newStudent = await Student.create({
+    //   firstName: "Bobbie",
+    //   school: newSchool._id,
+    // });
 
-    // query with the school
-    const match = await Student.findById(newStudent.id)
-      .populate("school") // creates a sort of virtual join table
+    // // query with the school
+    // const match = await Student.findById(newStudent.id)
+    //   .populate("school") // creates a sort of virtual join table
+    //   .exec();
+    //console.log(match);
+    const schoolConfig = {
+      name: "Cool School",
+      openSince: 2009,
+      students: 1100,
+      isGreat: true,
+      staff: ["a", "b", "c"],
+    };
+    const schoolConfig2 = {
+      name: "Another School",
+      openSince: 1980,
+      students: 600,
+      isGreat: false,
+      staff: ["d", "e", "f"],
+    };
+
+    const schools = await School.create([schoolConfig, schoolConfig2]);
+    const match = await School.findOne({
+      students: {
+        $gt: 600, // $gt means greater than and comes from Mongo, not Mongoose
+      },
+      staff: "b", // mongoose is smart enough to look in the array
+      // other operators include
+      // $in: {staff: ['d', 'e']}
+    })
+      .sort({openSince: -1}) // or 1 for ascending
+      .limit(2)
       .exec();
 
     console.log(match);
+    // const school = School.findOneAndUpdate(
+    //   {name: "cool school"},
+    //   {name: "cool school"},
+    //   {upsert: true, new: true}
+    // ).exec();
   })
   .catch((e) => console.error(e));
